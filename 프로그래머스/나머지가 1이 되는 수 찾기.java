998313095d1e661b79135c0bc3e05695bb98357e
dlwlsk0428 <84066919+dlwlsk0428@users.noreply.github.com>
2022-11-04 00:00:29 +0900

    Create 나머지가 1이 되는 수 찾기.java

---
+++ 프로그래머스/나머지가 1이 되는 수 찾기.java
@@ -0,0 +1,17 @@
+/**
+자연수 n이 매개변수로 주어집니다. 
+n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 return 하도록 solution 함수를 완성해주세요. 
+답이 항상 존재함은 증명될 수 있습니다.
+
+제한사항
+3 ≤ n ≤ 1,000,000
+*/
+
+class Solution {
+    public int solution(int n) {
+        for(int i = 2; i<n-1; i++) {
+            if((n-1)%i == 0) return i;
+        }
+        return n-1;
+    }
+}
