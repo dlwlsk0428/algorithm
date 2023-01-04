c7cacd65c16e7aa0f52d38d38696db2bae766bbb
dlwlsk0428 <84066919+dlwlsk0428@users.noreply.github.com>
2023-01-04 20:59:23 +0900

    Create 마법의 엘리베이터.java

---
+++ 프로그래머스/마법의 엘리베이터.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int storey) {
+        int answer = 0;
+        while(storey > 0) {
+            if(storey % 10 > 5) {
+                answer += 10 - storey % 10;
+                storey += 10;
+            } else if(storey % 10 == 5 && storey %100 >= 50) {
+                answer += 10 - storey % 10;
+                storey += 10;
+            }
+            else answer += storey % 10;
+            storey /= 10;
+        }
+        return answer;
+    }
+}
