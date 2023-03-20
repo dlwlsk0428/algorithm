331ebf4f96f37a352c734d635e6143078a42dac1
dlwlsk0428 <84066919+dlwlsk0428@users.noreply.github.com>
2023-03-21 04:32:10 +0900

    Create Main_2225_합분해.java

---
+++ 백준/골드/Main_2225_합분해.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_2225_합분해 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		long dp[][] = new long[N+1][K+1];
+		
+		for(int i = 0; i<=N; i++) dp[i][1] = 1;
+		for(int i = 1; i<=K; i++) dp[0][i] = 1;
+		
+		for(int i = 1; i<=N; i++) {
+			for(int j = 2; j<=K; j++) {
+				dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000000;
+			}
+		}
+		
+		System.out.println(dp[N][K]);
+
+	}
+}
